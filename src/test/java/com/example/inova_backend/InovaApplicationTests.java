49aa3fda9009ffde7a30539d30cdddce8e6b9641
Pedro Bonini <phf_apps@outlook.com>
2025-05-01 22:41:18 -0300

    inicial commit

---
+++ src/test/java/com/example/inova_backend/InovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inova_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
