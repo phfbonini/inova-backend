5095c8122aab10d8470eff01d35bfe80c805f1b2
Pedro Bonini <phf_apps@outlook.com>
2025-06-04 17:46:16 -0300

    refactor: passando usuario id em pessoa

--- src/main/java/com/example/inova_backend/dto/PessoaDTO.java
+++ src/main/java/com/example/inova_backend/dto/PessoaDTO.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class PessoaDTO {
     private Long id;
     private String nome;
+    private Long usuarioId;
     private String documentoNumero;
     private String tipoPessoa;
     private String telefone;
